5bd1de2b1d18db0f9900dfbf22ee53946cdec8f3
Marcel Koch <kontak@marcelkoch.net>
2015-10-03 07:43:34 +0200

    Cleaning

--- almostthere/MKNJobsManager.m
+++ almostthere/MKNJobsManager.m
@@ -7,29 +7,16 @@
 //
 
 #import "MKNJobsManager.h"
-#import "Parse/Parse.h"
 
 @implementation MKNJobsManager
 
 -(void) saveJob
 {
-    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
-    testObject[@"foo"] = @"bar";
-    [testObject saveInBackground];
 }
 
 -(void) printJobs
 {
-    PFQuery *query = [PFQuery queryWithClassName:@"TestObject"];
-    
-    NSArray *objects = [query findObjects];
-    
-    // The find succeeded.
-    NSLog(@"Successfully retrieved %d scores.", objects.count);
-    // Do something with the found objects
-    for (PFObject *object in objects) {
-        NSLog(@"%@", object.objectId);
-    }
+
 }
 
 @end
